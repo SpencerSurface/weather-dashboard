da57a07d8ba990ea659a31b648705826fa6a50ae
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2023-12-19 18:27:46 -0500

    initialize JS, add event listener for submit event

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Select relevant elements
+const formEl = document.querySelector("form");
+
+// Initialize, set up event listeners
+formEl.addEventListener("submit", handleSubmit);
+
+
+// Function definitions
+// Handle the form submit event
+function handleSubmit(event) {
+    event.preventDefault();
+}
