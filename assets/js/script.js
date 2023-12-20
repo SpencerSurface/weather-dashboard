152e7736ae7db9e62313534d06657b724873e549
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2023-12-20 16:08:07 -0500

    add storage functionality, add initial code for displaying the weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const apiKey = "b2fdb0743998319a111e3826d228b094";
 
 // Select relevant elements
 const formEl = document.querySelector("form");
+const forecastRowEl = document.querySelector("#forecast-row")
 
 // Initialize, set up event listeners
 formEl.addEventListener("submit", handleSubmit);
@@ -25,9 +26,6 @@ function handleSubmit(event) {
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey).then(jsonifyResponse)
     .then(getCoords)
     .then(fetchWeather)
-    .catch(function () {
-        alert("Error");
-    })
 }
 
 
@@ -56,20 +54,63 @@ function getCoords(data) {
 
 
 function fetchWeather() {
-    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey).then(jsonifyResponse)
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial").then(jsonifyResponse)
     .then(storeWeather)
     .then(displayWeather)
-    .catch(function () {
-        alert("Error");
-    })
 }
 
 
 function storeWeather(weather) {
+    localStorage.setItem(weather.city.name, JSON.stringify(weather));
     return weather;
 }
 
 
 function displayWeather(weather) {
+    forecastRowEl.innerHTML = "<div class='col-12'><h3>5-Day Forecast:</h3></div>";
+
+    // TODO: replace hardcoded values
+    document.querySelector("#current-city").textContent = "Atlanta";
+    document.querySelector("#current-date").textContent = "12/12/2023";
+    document.querySelector("#current-icon").textContent = "*";
+    document.querySelector("#current-temp").textContent = "53F";
+    document.querySelector("#current-wind").textContent = "8mph";
+    document.querySelector("#current-humidity").textContent = "71%";
+
+
+    // TODO: replace hardcoded values
+    let icon = "*"
+    let temp = "53F";
+    let wind = "10mph";
+    let humidity = "53%";
+
+    for (let i = 0; i < 5; i++) {
+        let forecastCard = document.createElement("div");
+        forecastCard.classList.add("col", "card", "me-3", "px-2");
+
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        let cardHeading = document.createElement("h4");
+        cardHeading.classList.add("card-title");
+        cardHeading.textContent = "12/12/2023";
+        let cardIcon = document.createElement("p");
+        cardIcon.classList.add("card-text");
+        cardIcon.textContent = icon;
+        let cardTemp = document.createElement("p");
+        cardTemp.classList.add("card-text");
+        cardTemp.textContent = "Temp: " + temp;
+        let cardWind = document.createElement("p");
+        cardWind.classList.add("card-text");
+        cardWind.textContent = "Wind: " + wind;
+        let cardHumidity = document.createElement("p");
+        cardHumidity.classList.add("card-text");
+        cardHumidity.textContent = "Humidity: " + humidity;
+
+        cardBody.append(cardHeading, cardIcon, cardTemp, cardWind, cardHumidity);
+        forecastCard.appendChild(cardBody);
+        forecastRowEl.appendChild(forecastCard);
+    }
+
     console.log(weather);
 }
